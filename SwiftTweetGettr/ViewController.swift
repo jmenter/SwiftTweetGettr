07db60173c96b81d1cdc8a9031325e040dec8e13
Jeff Menter <jmenter@gmail.com>
2014-06-05 19:28:58 -0500

    added some extensions and removed some semicolons

--- SwiftTweetGettr/ViewController.swift
+++ SwiftTweetGettr/ViewController.swift
@@ -1,18 +1,57 @@
 
 import UIKit
 
+extension NSURLResponse {
+    
+    func httpResponseIsValid() -> Bool {
+        let httpResponse = self as? NSHTTPURLResponse
+        return (httpResponse?.statusCode >= 200 && httpResponse?.statusCode <= 299)
+    }
+}
+
+extension NSData {
+    
+    func json() -> AnyObject {
+        return NSJSONSerialization.JSONObjectWithData(self, options: nil, error: nil)
+    }
+}
+
+extension UITableView {
+    
+    func scrollToTop(animated: Bool) {
+        self.scrollToRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0),
+            atScrollPosition: UITableViewScrollPosition.Top, animated: animated)
+    }
+}
+
+extension UIViewController {
+    
+    func showAlertViewWithMessage(message : String) {
+        var alertController = UIAlertController(title: "Oops!", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+}
+
+extension String {
+    
+    func base64Encoded() -> String {
+        return self.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false).base64Encoding()
+    }
+}
+
 class ViewController : UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
     
     let kAPIKey = "";
     let kAPISecret = "";
-    let kPostMethod = "POST";
-    let kGetMethod = "GET";
-    let kContentTypeHeader = "Content-Type";
-    let kAuthorizationHeader = "Authorization";
-    let kOAuthRootURL = "https://api.twitter.com/oauth2/token";
-    let kTimelineRootURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=30&screen_name=";
-    let kAuthorizationBody = "grant_type=client_credentials";
-    let kAuthorizationContentType = "application/x-www-form-urlencoded;charset=UTF-8";
+    let kPostMethod = "POST"
+    let kGetMethod = "GET"
+    let kContentTypeHeader = "Content-Type"
+    let kAuthorizationHeader = "Authorization"
+    let kOAuthRootURL = "https://api.twitter.com/oauth2/token"
+    let kTimelineRootURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=30&screen_name="
+    let kAuthorizationBody = "grant_type=client_credentials"
+    let kAuthorizationContentType = "application/x-www-form-urlencoded;charset=UTF-8"
 
     var spinner : UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
     var tweets = NSArray()
@@ -60,43 +99,36 @@ class ViewController : UIViewController, UITableViewDataSource, UITableViewDeleg
         tokenRequest.HTTPMethod = kPostMethod
         tokenRequest.HTTPBody = kAuthorizationBody.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
         tokenRequest.addValue(kAuthorizationContentType, forHTTPHeaderField: kContentTypeHeader)
-        tokenRequest.addValue(authorizationString(), forHTTPHeaderField: kAuthorizationHeader)
+        tokenRequest.addValue("Basic " + (kAPIKey + ":" + kAPISecret).base64Encoded(), forHTTPHeaderField: kAuthorizationHeader)
         
         NSURLConnection.sendAsynchronousRequest(tokenRequest, queue: NSOperationQueue.mainQueue(), completionHandler:{ (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-            let httpResponse = response as? NSHTTPURLResponse
-            if (httpResponse?.statusCode >= 200 && httpResponse?.statusCode <= 299) {
-                var json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSDictionary
+            if response.httpResponseIsValid() {
+                var json = data.json() as NSDictionary
                 AppDelegate.shared().authorizationToken = json["access_token"] as? String
                 self.fetchTweets()
             } else {
-                self.showAlertViewWithMessage("something went wrong getting access token")
+                self.showAlertViewWithMessage("Something went wrong getting access token.")
             }
 
         })
         spinner.stopAnimating()
     }
 
-    func authorizationString() -> String {
-        return "Basic " + (kAPIKey + ":" + kAPISecret).dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false).base64Encoding()
-    }
-
     func fetchTweets() {
         var tweetRequest = NSMutableURLRequest(URL: NSURL(string: kTimelineRootURL + textField.text))
         tweetRequest.HTTPMethod = kGetMethod;
         tweetRequest.addValue("Bearer " + AppDelegate.shared().authorizationToken!, forHTTPHeaderField: kAuthorizationHeader)
+        
         NSURLConnection.sendAsynchronousRequest(tweetRequest, queue: NSOperationQueue.mainQueue(), completionHandler:{ (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-            let httpResponse = response as? NSHTTPURLResponse
-            if (httpResponse?.statusCode >= 200 && httpResponse?.statusCode <= 299) {
-                self.tweets = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSArray
+            if response.httpResponseIsValid() {
+                self.tweets = data.json() as NSArray
                 self.tableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: UITableViewRowAnimation.Fade)
-                self.tableView.scrollToRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0), atScrollPosition: UITableViewScrollPosition.Top, animated: true)
+                self.tableView.scrollToTop(true)
             } else {
-                self.showAlertViewWithMessage("something went wrong getting tweets")
+                self.showAlertViewWithMessage("Something went wrong getting tweets.")
             }
-            
-            })
+        })
         spinner.stopAnimating()
-
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -112,16 +144,8 @@ class ViewController : UIViewController, UITableViewDataSource, UITableViewDeleg
         return cell
     }
 
-
-    func showAlertViewWithMessage(message : String) {
-        var alertController = UIAlertController(title: "Oops!", message: message, preferredStyle: UIAlertControllerStyle.Alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-        presentViewController(alertController, animated: true, completion: nil)
-    }
-
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
             let tweet = tweets[tableView.indexPathForSelectedRow().row] as NSDictionary
-            
             (segue.destinationViewController.view as UITextView).text = tweet.description
     }
     
