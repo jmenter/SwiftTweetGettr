bc8ee9ebcbd7a3d98b65730b3fa4e2aaf1ea7cca
Jeff Menter <jmenter@gmail.com>
2014-06-05 21:15:17 -0500

    refactored tableview datasource and delegate to new file

---
+++ SwiftTweetGettr/TweetsTableViewDelegate.swift
@@ -0,0 +1,22 @@
+
+import UIKit
+
+class TweetsTableViewDelegate : NSObject, UITableViewDataSource, UITableViewDelegate {
+ 
+    var tweets = NSArray()
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweets.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("tweetCell", forIndexPath: indexPath) as UITableViewCell
+        let tweet = tweets[indexPath.row] as NSDictionary
+        
+        cell.textLabel.text = tweet["text"] as String
+        cell.detailTextLabel.text = tweet["created_at"] as String
+        return cell
+    }
+    
+
+}
