6d1c9ca5a05ab195495794b33e4c67c849613d2f
Jeff Menter <jmenter@gmail.com>
2014-12-12 15:17:22 -0600

    made greener

--- SwiftTweetGettr/AppDelegate.swift
+++ SwiftTweetGettr/AppDelegate.swift
@@ -6,4 +6,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool
+    {
+        let greenHue:CGFloat = 123.0/360.0
+        self.window?.tintColor = UIColor(hue: greenHue, saturation: 0.5, brightness: 0.5, alpha: 1.0)
+        UINavigationBar.appearance().barTintColor = UIColor(hue: greenHue, saturation: 0.05, brightness: 0.95, alpha: 1.0)
+        return true
+    }
 }
